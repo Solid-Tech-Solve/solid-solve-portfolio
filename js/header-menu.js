226b9db9f2d3e6f5041ee5bafc27e03e09cb8bd0
Matheus Almeida <saagas.code@gmail.com>
2024-08-07 20:22:56 -0300

    added mobile menu on header

---
+++ js/header-menu.js
@@ -0,0 +1,10 @@
+let navMobile = document.querySelector(".nav-mobile")
+let menuButton = document.querySelector(".menu-button")
+let closeButton = document.querySelector(".close")
+
+menuButton.addEventListener('click', () => toggleMenuMobile())
+closeButton.addEventListener('click', () => toggleMenuMobile())
+
+const toggleMenuMobile = () => {
+    navMobile.classList.toggle('opened')
+}
