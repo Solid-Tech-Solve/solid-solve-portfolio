e38f2b223f833255a6f8b63ac052a3a6b901fe7c
Everton Tajano <evertontrajano@solidsolve.net>
2024-09-01 01:08:13 -0300

    refactored and added build system

---
+++ index.js
@@ -0,0 +1,124 @@
+import { createReadStream, createWriteStream, existsSync, mkdirSync, readFileSync } from 'node:fs';
+import { pipeline } from 'stream/promises';
+import { loadEnvFile } from 'node:process';
+import { join, resolve } from 'node:path';
+import { createGzip, createBrotliCompress } from 'node:zlib';
+import { cp, rm } from 'node:fs/promises';
+import { Readable } from 'node:stream';
+
+loadEnvFile('./.env');
+const cssRegex = RegExp(/\<link.*rel=.stylesheet..*(.*)\>/);
+const svgRegex = RegExp(/\<img.*(.svg).*\>/);
+const jsRegex = RegExp(/\<script.*src=.*\>/);
+const { ENTRY_FILE, ENTRY_DIR, OUTPUT_DIR, OUTPUT_FILE } = process.env;
+
+const resolveIfRegexIsValid = (regex, value, _true) => regex.test(value) ? _true(value) : value;
+
+async function* convertReadStreamToLines(source) {
+    for await (const file of source) {
+        for await (const line of file.split('\n')) yield line + '\n';
+    }
+}
+
+function findAndCopyFileByPattern(entryCondition, pattern, externalCondition = false) {
+    return async function* (chunk) {
+        for await (const line of chunk) {
+            if (line.match(entryCondition) || externalCondition) {
+                const svgRegex = RegExp(pattern)
+                if (svgRegex.test(line)) {
+                    const fixedRef = svgRegex.exec(line)[1].replace("../", "./");
+                    cp(resolve(ENTRY_DIR, fixedRef), resolve(OUTPUT_DIR, fixedRef), { recursive: true });
+                }
+            }
+            yield line
+        }
+    }
+}
+
+async function* performCss(chunk) {
+    for await (const ref of chunk) {
+        yield resolveIfRegexIsValid(cssRegex, ref, async (line) => {
+            const [_, fileRef] = /href=["'](.*)["']/.exec(line)
+            const content = readFileSync(resolve(ENTRY_DIR, fileRef), { encoding: 'utf8' });
+            setTimeout(() => pipeline(
+                Readable.from(content.split('\n')),
+                findAndCopyFileByPattern(/@font-face/, /src:.*url\(['"](.*)['"]\)\s/, content.match(/@font-face/)),
+                findAndCopyFileByPattern(/content:/, /url\(['"](.*)['"]\)/)
+            ), 0)
+
+            return `<style>\n${content}</style>\n`;
+        })
+    }
+}
+
+async function* performSvg(chunk) {
+    for await (const ref of chunk) {
+        yield resolveIfRegexIsValid(svgRegex, ref, (line) => {
+            const isEager = /loading="eager"/.test(line);
+
+            const result = /<img\s+(?=.*?src="([^"]*)")(?=.*?class="([^"]*)")?(?=.*?id="([^"]*)")?[^>]*>/g.exec(line)
+            const [got, fileRef] = result
+            if (!isEager) {
+                cp(resolve(ENTRY_DIR, fileRef), resolve(OUTPUT_DIR, fileRef), { recursive: true })
+                // setTimeout(() => pipeline(
+                //     Readable.from(content.split('\n')),
+                //     chooseCompression('brotli'),
+                //     createWriteStream()
+                // ), 0)
+                return line
+            };
+            const className = /class=["']([\d\w]*)["'] /.exec(line)
+            const id = /id=["']([\d\-\w]*)["'] /.exec(line)
+            const content = readFileSync(resolve(ENTRY_DIR, fileRef), { encoding: 'utf8' });
+
+            const chunk = '\n' + content.replace('\n', '').replace(/(<svg)/, `$1 id="${id?.at(1) || ""}" class="${className?.at(1) || ""}"`) + '\n'
+            return line.replace(got, chunk)
+        })
+    }
+}
+
+async function* performJS(chunk) {
+    for await (const ref of chunk) {
+        yield resolveIfRegexIsValid(jsRegex, ref, (line) => {
+            const [_, fileRef] = /src=["'](.*)["']/.exec(line)
+            if (fileRef.match('http')) return line;
+
+            const content = readFileSync(resolve(ENTRY_DIR, fileRef), { encoding: 'utf8' }).replace('\n', ' ');
+            return `<script>\n${content}</script>\n`
+        })
+    }
+}
+
+function chooseCompression(compressionType) {
+    switch (compressionType) {
+        case 'gzip':
+            return createGzip({ level: 5 });
+        case 'brotli':
+            return createBrotliCompress({})
+        default:
+            return function* (chunk) { yield chunk }
+    }
+}
+
+async function main() {
+    const entryFileExists = existsSync(join(ENTRY_DIR, ENTRY_FILE));
+    if (!entryFileExists) throw new Error('Entry file does not exists.');
+
+    const outputDirExists = existsSync(resolve(OUTPUT_DIR))
+    if (outputDirExists) {
+        await rm(resolve(OUTPUT_DIR) + '/', { recursive: true });
+    }
+    mkdirSync(resolve(OUTPUT_DIR))
+
+    await pipeline(
+        createReadStream(join(ENTRY_DIR, ENTRY_FILE), { encoding: 'utf8' }),
+        convertReadStreamToLines,
+        performCss,
+        performSvg,
+        performJS,
+        chooseCompression('brotli'),
+        createWriteStream(join(OUTPUT_DIR, OUTPUT_FILE + '.br'))
+    )
+}
+
+main();
--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -1,22 +1,10 @@
-let navMobile = document.querySelector(".nav-mobile")
-let menuButton = document.querySelector(".menu-button")
-let closeButton = document.querySelector(".close")
+let navMobile = document.querySelector(".nav-mobile");
+let menuButton = document.querySelector(".menu-button");
+let closeButton = document.querySelector(".close");
 
-menuButton.addEventListener('click', () => toggleMenuMobile())
-closeButton.addEventListener('click', () => toggleMenuMobile())
+menuButton.addEventListener('click', () => toggleMenuMobile());
+closeButton.addEventListener('click', () => toggleMenuMobile());
 
 const toggleMenuMobile = () => {
-    navMobile.classList.toggle('opened')
-}
-
-const changeHeader = (string) => {
-    let header = document.querySelector("header")
-    let divTest = document.querySelector("#div-test")
-    if(!divTest) {
-        header.insertAdjacentHTML('afterend', '<div id="div-test"></div>');
-    }
-    let divTest2 = document.querySelector("#div-test")
-    divTest2.innerText = string
-    console.log(divTest2)
-}
-// changeHeader(window.devicePixelRatio)
+    navMobile.classList.toggle('opened');
+};
